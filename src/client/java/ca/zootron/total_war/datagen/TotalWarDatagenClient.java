d8ecbed76acf3e7ebf2e3a8467be7eb08978be47
Justin Hu <justin.hu@alumni.ubc.ca>
2022-12-21 22:47:28 -0800

    Blank patchouli book

--- src/client/java/ca/zootron/total_war/datagen/TotalWarDatagenClient.java
+++ src/client/java/ca/zootron/total_war/datagen/TotalWarDatagenClient.java
@@ -1,17 +1,17 @@
 package ca.zootron.total_war.datagen;
 
-import static ca.zootron.total_war.TotalWar.MODID;
 
+import ca.zootron.total_war.TotalWar;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TotalWarDatagenClient implements DataGeneratorEntrypoint {
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(TotalWar.MODID);
 
     @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        LOGGER.info("Initialized {} datagen client", MODID);
+    public void onInitializeDataGenerator(FabricDataGenerator gen) {
+        LOGGER.info("Initialized {} datagen client", TotalWar.MODID);
     }
 }
